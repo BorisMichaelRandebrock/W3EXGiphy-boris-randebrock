b74b439ddbd89e23460d6f08dce78abbee7e8971
Boris <boris@randebrock.com>
2022-04-22 16:22:41 +0200

    all wrong

---
+++ index.js
@@ -0,0 +1,6 @@
+const apiKey = "VAOANbkkh8QmzzZEYmGiCgxfoQHjQrfh";
+
+const gifs = api.giphy.com / v1 / gifs / random;
+const stickers = "api.giphy.com/v1/stickers/random";
+const apiKeyKittens =
+  "https://api.giphy.com/v1/gifs/random?api_key=VAOANbkkh8QmzzZEYmGiCgxfoQHjQrfh&tag=kitten";
